d606b816bae4b2dc0a6c18877050cf7cf4b02d77
sham <khaj3172@gmail.com>
2023-06-23 14:18:03 +0500

    first Time upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Box, Typography, Button } from '@mui/material'
+import React from 'react'
+
+
+const App = () => {
+  return (
+    <Box p={2} bgcolor='primary.main' color='primary.contrastText'>
+      <Typography variant="h3" color="initial">Vite + React + MUI + Redux  </Typography>
+    </Box>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { store } from './app/store.jsx'
+import { Provider } from 'react-redux'
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme.jsx'
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+      <CssBaseline />
+      <Provider store={store}>
+    <App />
+  </Provider>
+    </ThemeProvider>
+  </React.StrictMode>,
+)
